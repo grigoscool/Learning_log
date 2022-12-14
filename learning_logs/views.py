72d9024c32f2d9add17711e1dc9aedb6c2416bc9
Sergey Grigorev <grigoscool@mail.ru>
2022-12-14 19:07:50 +0300

    django project, add form

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Topic
-
+from .forms import TopicForm
 
 def index(reguest):
     """Домашняя страница приложения learning_logs"""
@@ -19,3 +19,17 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     content = {'topics': topics, 'entries': entries}
     return render(request, 'learning_logs/topic.html', content)
+
+def new_topic(request):
+    """Определяет новую тему"""
+    if request.method != 'POST':
+        # Данные не отправлялись, создается пустая форма
+        form = TopicForm()
+    else:
+        # Отправленные данные POST, обработать данные
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
